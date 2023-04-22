0d278da9feb1316e06446922db441387549ba196
Conrado Gomes <gomes.conrado@gmail.com>
2023-04-21 23:50:14 -0300

    Adivinhação atualizado

--- comecandoComALinguagem/adivinhacao.py
+++ comecandoComALinguagem/adivinhacao.py
@@ -3,14 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto= 42
+total_de_tentativas = 3
+rodadas = 1
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+while (rodadas <= total_de_tentativas):
+    print("Tentativa {} de {}.".format(rodadas,total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-if (chute==numero_secreto):
-    print("Você acertou")
-else:
-    print("Você errou")
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou")
+        break
+    elif(maior):
+        print("Você errou. Seu chute foi maior que o número secreto.")
+        rodadas +=1
+    else:
+        print("Você errou. Seu chute foi menor que o número secreto.")
+        rodadas +=1
 
 print("Fim de jogo")
