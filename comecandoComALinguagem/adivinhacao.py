3f7d06e8f1b47b6af1194780027fd241c9966866
Conrado Gomes <gomes.conrado@gmail.com>
2023-04-24 10:41:37 -0300

    Adicionado níveis de dificuldade

--- comecandoComALinguagem/adivinhacao.py
+++ comecandoComALinguagem/adivinhacao.py
@@ -4,15 +4,27 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto= random.randrange(1,101)
-total_de_tentativas = 3
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
+
+print("Qual nível de dificuldade ?")
+print("(1) Fácil, (2) Médio, (3) Difícil")
+
+nivel = int(input("Define o nível"))
+
+if (nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas+1):
-    print("Tentativa {} de {}.".format(rodada,total_de_tentativas))
+    print("Tentativa {} de {}.".format(rodada, total_de_tentativas))
     chute_str = input("Digite o seu número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
-    if(chute <0 or chute>100):
+    if(chute < 0 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
