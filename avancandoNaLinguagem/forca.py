3984f9be48d6607debaa1ba6c17040d7a34a9fca
Conrado Gomes <gomes.conrado@gmail.com>
2023-04-25 01:01:16 -0300

    Começando a tratar as entradas

--- avancandoNaLinguagem/forca.py
+++ avancandoNaLinguagem/forca.py
@@ -5,9 +5,17 @@ def jogar():
 
     palavra_secreta = "banana"
     enforcou = False
-    acertou = True
+    acertou = False
 
-    while(not acertou and not enforcou)
+    while(not acertou and not enforcou):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(chute, index))
+            index += 1
+
         print("Jogando...")
 
 if (__name__ == "__main__"):
