a7a1117e64ea5982e8b88f31fc834e1cba741fdc
Conrado Gomes <gomes.conrado@gmail.com>
2023-04-25 00:06:26 -0300

    Iniciando o jogo de forca

---
+++ avancandoNaLinguagem/forca.py
@@ -0,0 +1,14 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = True
+
+    while(not acertou and not enforcou)
+        print("Jogando...")
+
+if (__name__ == "__main__"):
+    jogar()
